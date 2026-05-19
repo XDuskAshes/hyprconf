85409101aac9f60c42f77a6d6545d77934f82402
XDuskAshes <xduskashesreal@gmail.com>
2026-05-18 23:29:54 -0400

    make all onStart programs die instead of just elephant and walker

--- mod/autostart.lua
+++ mod/autostart.lua
@@ -14,8 +14,13 @@ hl.on("config.reloaded", function()
     util.notify("Hyprland", "Config has been reloaded.")
     hl.exec_cmd("pkill " .. programs.uishell .. " && " .. programs.quickshell)
     hl.exec_cmd("pkill " .. programs.wallpaper_manager .. " && " .. programs.wallpaper_manager)
+    hl.exec_cmd("pkill " .. programs.onStart.notif_daemon .. " && " .. programs.onStart.notif_daemon)
 end)
 
 hl.on("hyprland.shutdown", function()
-    hl.exec_cmd("pkill elephant && pkill walker")
+    for _, prog in pairs(programs.onStart) do
+        local n = prog:match("^[^ ]+")
+        hl.exec_cmd("pkill " .. n)
+        print("Killing " .. n)
+    end
 end)

fa7e842e312cd491187551e57229456136edf6b1
XDuskAshes <xduskashesreal@gmail.com>
2026-05-18 23:29:27 -0400

    make mako look pretty :3

--- mod/programs.lua
+++ mod/programs.lua
@@ -1,3 +1,5 @@
+local colors = require("mod.mocha")
+
 return {
     fileManager = "dolphin",
     menu        = "walker",
@@ -10,11 +12,12 @@ return {
     screenshot  = {
         monitor = "hyprshot -m output --raw | satty --filename -",
         window = "hyprshot -m window --raw | satty --filename -",
-        selection = "hyprshot -m region --raw | satty --filename -"
+        selection = "hyprshot -m region -- raw | satty --filename -"
     },
     onStart     = { -- Stuff that needs to get running immediately
         uishell             = "quickshell",
-        notif_daemon        = "mako",
+        notif_daemon        = "mako --default-timeout 3500 --background-color \"" ..
+            colors.surface2 .. "\" --border-color \"" .. colors.lavender .. "\"",
         wallpaper_manager   = "hyprpaper",
         audioeffectsmanager = "easyeffects -w",
         passwordmanager     = "keepassxc"
